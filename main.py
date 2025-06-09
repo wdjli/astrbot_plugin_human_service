4e283dfd5ce6b33a6094e8dc4ea5e0f508501888
Zhalslar <zsl2936169201@gmail.com>
2025-06-10 02:21:34 +0800

    正式发布

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import re
 from astrbot.api.event import filter
 from astrbot.api.star import Context, Star, register
 from astrbot.core.config.astrbot_config import AstrBotConfig
+from astrbot.core.message.components import Reply
 from astrbot.core.platform.sources.aiocqhttp.aiocqhttp_message_event import (
     AiocqhttpMessageEvent,
 )
@@ -10,18 +12,18 @@ from astrbot.core.platform.sources.aiocqhttp.aiocqhttp_message_event import (
     "astrbot_plugin_human_service",
     "Zhalslar",
     "人工客服插件",
-    "1.0.1",
+    "1.0.2",
     "https://github.com/Zhalslar/astrbot_plugin_human_service",
 )
 class HumanServicePlugin(Star):
     def __init__(self, context: Context, config: AstrBotConfig):
         super().__init__(context)
-        self.admin_id: str = config.get("admin_id", "")
-        if not self.admin_id:
+        self.servicers_id: list[str] = config.get("servicers_id", "")
+        if not self.servicers_id:
             for admin_id in context.get_config()["admins_id"]:
                 if admin_id.isdigit():
-                    self.admin_id = admin_id
-                    break
+                    self.servicers_id.append(admin_id)
+
         self.session_map = {}
         self.prefix: list[str] = context.get_config()["wake_prefix"][0]
 
@@ -36,144 +38,90 @@ class HumanServicePlugin(Star):
             return
 
         self.session_map[sender_id] = {
-            "admin": self.admin_id,
+            "servicer_id": "",
             "status": "waiting",
             "group_id": group_id,
         }
+        yield event.plain_result("正在等待客服👤接入...")
+        for servicer_id in self.servicers_id:
+            await self.send(
+                event,
+                message=f"{send_name}({sender_id}) 请求转人工",
+                user_id=servicer_id,
+            )
 
-        reply = f"用户🗣{send_name}({sender_id}) 请求转人工\n请发送 {self.prefix}接入对话 {sender_id}"
-        await self.send(event, message=reply, user_id=self.admin_id)
-        yield event.plain_result("正在等待客服👤转接...")
 
     @filter.command("转人机", priority=1)
-    async def cancel_wait(self, event: AiocqhttpMessageEvent):
+    async def transfer_to_bot(self, event: AiocqhttpMessageEvent):
         sender_id = event.get_sender_id()
         sender_name = event.get_sender_name()
         session = self.session_map.get(sender_id)
 
         if session and session["status"] == "connected":
-            del self.session_map[sender_id]
             await self.send(
                 event,
                 message=f"❗{sender_name} 已取消人工请求",
-                user_id=self.admin_id,
+                user_id=session["servicer_id"],
             )
+            del self.session_map[sender_id]
             yield event.plain_result("好的，我现在是人机啦！")
 
-
     @filter.command("接入对话", priority=1)
-    async def accept_conversation(self, event: AiocqhttpMessageEvent):
-        if not event.is_admin():
+    async def accept_conversation(
+        self, event: AiocqhttpMessageEvent, target_id: str | int | None = None
+    ):
+        sender_id = event.get_sender_id()
+        if sender_id not in self.servicers_id:
             return
-        target_id = event.message_str.split()[1]
+
+        if reply_seg := next(
+            (seg for seg in event.get_messages() if isinstance(seg, Reply)), None
+        ):
+            if text := reply_seg.message_str:
+                if match := re.search(r"\((\d+)\)", text):
+                    target_id = match.group(1)
+
         session = self.session_map.get(target_id)
 
         if not session or session["status"] != "waiting":
-            yield event.plain_result("❎ 用户不存在或未请求人工")
+            yield event.plain_result(f"用户({target_id})未请求人工")
             return
 
         if session["status"] == "connected":
-            yield event.plain_result("❎ 您正在与该用户对话")
+            yield event.plain_result("您正在与该用户对话")
 
         session["status"] = "connected"
+        session["servicer_id"] = sender_id
+
         await self.send(
             event,
             message="管理员👤已接入",
             group_id=session["group_id"],
             user_id=target_id,
         )
-        yield event.plain_result(
-            f"☑ 已接入, 接下来我将会把你的消息转发给对方\n"
-            f"{self.prefix}暂停对话 {target_id} \n"
-            f"{self.prefix}结束对话"
-        )
+        yield event.plain_result("好的，接下来我将转发你的消息给对方，请开始对话：")
         event.stop_event()
 
-    @filter.command("暂停对话", priority=1)
-    async def pause_conversation(self, event: AiocqhttpMessageEvent):
-        sender_id = event.get_sender_id()
-        if sender_id != self.admin_id:
-            return
-
-        target_id = event.message_str.split()[1]
-        session = self.session_map.get(target_id)
-
-        if not session:
-            yield event.plain_result("❎ 请输入需要暂停的用户id")
-            return
-
-        if session["status"] == "connected":
-            session["status"] = "paused"
-            await self.send(
-                event,
-                message="⚠ 管理员👤暂停了对话",
-                group_id=session["group_id"],
-                user_id=target_id,
-            )
-            yield event.plain_result(f"✅ 已暂停与用户 {target_id} 的对话")
-        else:
-            yield event.plain_result("❎ 无法暂停：用户不存在或未处于对话中")
-
-    @filter.command("恢复对话", priority=1)
-    async def resume_conversation(self, event: AiocqhttpMessageEvent):
-        sender_id = event.get_sender_id()
-        if sender_id != self.admin_id:
-            return
-
-        target_id = event.message_str.split()[1]
-        session = self.session_map.get(target_id)
-
-        if session and session["status"] == "paused":
-            session["status"] = "connected"
-            await self.send(
-                event,
-                message="🔔 管理员👤已恢复对话，请继续",
-                group_id=session["group_id"],
-                user_id=target_id,
-            )
-            yield event.plain_result(f"✅ 已恢复与用户 {target_id} 的对话")
-        else:
-            yield event.plain_result("❎ 无法恢复：用户不存在或未处于暂停状态")
 
     @filter.command("结束对话")
     async def end_conversation(self, event: AiocqhttpMessageEvent):
         sender_id = event.get_sender_id()
-        session = self.session_map.get(sender_id)
+        if sender_id not in self.servicers_id:
+            return
 
-        # 用户主动结束会话
-        if session:
-            if session["status"] == "waiting":
-                del self.session_map[sender_id]
+        for uid, session in self.session_map.items():
+            if session["servicer_id"] == sender_id:
                 await self.send(
                     event,
-                    message=f"🔔 用户 {sender_id} 已取消转人工请求",
-                    user_id=self.admin_id,
+                    message="客服👤已结束对话",
+                    group_id=session["group_id"],
+                    user_id=uid,
                 )
-                yield event.plain_result("🆗 您已取消转人工请求")
+                del self.session_map[uid]
+                yield event.plain_result(f"已结束与用户 {uid} 的对话")
+                return
 
-            elif session["status"] in ["connected", "paused"]:
-                await self.send(
-                    event,
-                    message=f"🔔 用户 {sender_id} 已结束对话",
-                    user_id=self.admin_id,
-                )
-                del self.session_map[sender_id]
-                yield event.plain_result("🆗 您已结束对话")
-        # 管理员主动结束会话
-        else:
-            for uid, sess in self.session_map.items():
-                if sess["admin"] == sender_id:
-                    await self.send(
-                        event,
-                        message="🔔 管理员👤已结束对话",
-                        group_id=sess["group_id"],
-                        user_id=uid,
-                    )
-                    del self.session_map[uid]
-                    yield event.plain_result(f"✅ 已结束与用户 {uid} 的对话")
-                    return
-
-            yield event.plain_result("❎ 当前无对话需要结束")
+        yield event.plain_result("当前无对话需要结束")
 
     async def send(
         self,
@@ -191,14 +139,16 @@ class HumanServicePlugin(Star):
     @filter.event_message_type(filter.EventMessageType.ALL)
     async def handle_match(self, event: AiocqhttpMessageEvent):
         """监听对话消息转发"""
+        sender_id = event.get_sender_id()
         message_str = event.get_message_str()
 
-        # 管理员 → 用户
-        if event.is_private_chat() and event.is_admin():
-            # 查找管理员当前接入的用户
+        # 管理员 → 用户 (仅私聊生效)
+        if event.is_private_chat():
+            if message_str == "接入对话" or message_str == "结束对话":
+                return
             for user_id, session in self.session_map.items():
                 if (
-                    session["admin"] == self.admin_id
+                    session["servicer_id"] == sender_id
                     and session["status"] == "connected"
                 ):
                     await self.send(
@@ -212,11 +162,11 @@ class HumanServicePlugin(Star):
 
         # 用户 → 管理员
         else:
-            session = self.session_map.get(event.get_sender_id())
+            session = self.session_map.get(sender_id)
             if session and session["status"] == "connected":
                 await self.send(
                     event,
                     message=f"🗣：{message_str}",
-                    user_id=self.admin_id,
+                    user_id=session["servicer_id"],
                 )
                 event.stop_event()
